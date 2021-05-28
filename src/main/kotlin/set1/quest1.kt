36dbebc5cdb81c501385fb3c4aba2ff6522f35d0
Malte S <maltestellmacher@gmail.com>
2021-05-28 20:39:12 +0200

    added solution for s1q1

---
+++ src/main/kotlin/set1/quest1.kt
@@ -0,0 +1,38 @@
+package set1
+
+import Keys
+import friendBot
+import org.stellar.sdk.*
+
+fun main() {
+    val server = Server("https://horizon-testnet.stellar.org")
+    val questAccount = KeyPair.fromSecretSeed(Keys.quest1PrivateKey)
+
+    // Need helper account to create with exactly 1000XLM
+    val creatorAccountKeys = KeyPair.random()
+    println("helper: ${creatorAccountKeys.accountId}")
+    creatorAccountKeys.friendBot()
+    val creatorAccount = server.accounts().account(creatorAccountKeys.accountId)
+
+    val txBuilder = Transaction.Builder(creatorAccount, Network.TESTNET)
+        .setBaseFee(FeeBumpTransaction.MIN_BASE_FEE)
+        .setTimeout(180)
+
+    txBuilder.addOperation(
+        CreateAccountOperation.Builder(questAccount.accountId, "1000").build()
+    )
+
+    val transaction = txBuilder.build()
+    transaction.sign(creatorAccountKeys)
+
+
+    println("Executing tx..")
+    try {
+        val response = server.submitTransaction(transaction)
+        println("Success!")
+        println("txHash: ${response.hash}")
+    } catch (e: Exception) {
+        println("Something went wrong!")
+        println(e.message)
+    }
+}
