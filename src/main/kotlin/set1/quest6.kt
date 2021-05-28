e0482963654c7ae483a4e07b9ed947ce13b6220d
Malte S <maltestellmacher@gmail.com>
2021-05-28 23:49:20 +0200

    added solution for s1q6

---
+++ src/main/kotlin/set1/quest6.kt
@@ -0,0 +1,35 @@
+package set1
+
+import Keys
+import org.stellar.sdk.*
+
+fun main() {
+    val server = Server("https://horizon-testnet.stellar.org")
+    val questAccountKeys = KeyPair.fromSecretSeed(Keys.quest1PrivateKey)
+    val questAccount = server.accounts().account(questAccountKeys.accountId)
+
+    val issuerAccountKeys = KeyPair.fromSecretSeed(Keys.set1helperKey)
+
+    val txBuilder = Transaction.Builder(questAccount, Network.TESTNET)
+        .setBaseFee(FeeBumpTransaction.MIN_BASE_FEE)
+        .setTimeout(180)
+
+    val myAsset = Asset.createNonNativeAsset("COOL", issuerAccountKeys.accountId)
+
+    txBuilder.addOperation(
+        ManageSellOfferOperation.Builder(myAsset, AssetTypeNative(), "1", "0.0001").build()
+    )
+
+    val transaction = txBuilder.build()
+    transaction.sign(questAccountKeys)
+    println("Executing tx..")
+    try {
+        val response = server.submitTransaction(transaction)
+        println("Success!")
+        println("txHash: ${response.hash}")
+        println("result: ${response.resultXdr.get()}")
+    } catch (e: Exception) {
+        println("Something went wrong!")
+        println(e.message)
+    }
+}
