e8d9d51cbe5c57bd685d3bf8fa185974a26250fb
Malte S <maltestellmacher@gmail.com>
2021-05-29 16:31:44 +0200

    added solution for s2q6

---
+++ src/main/kotlin/set2/quest6.kt
@@ -0,0 +1,43 @@
+package set2
+
+import Keys
+import org.stellar.sdk.*
+
+fun main() {
+    val server = Server("https://horizon-testnet.stellar.org")
+    val questAccountKeys = KeyPair.fromSecretSeed(Keys.quest2PrivateKey)
+    val questAccount = server.accounts().account(questAccountKeys.accountId)
+
+    val helperAccountKeys = KeyPair.fromSecretSeed(Keys.set2helperKey)
+    val helperAccount = server.accounts().account(helperAccountKeys.accountId)
+
+    val txBuilder = Transaction.Builder(questAccount, Network.TESTNET)
+        .setBaseFee(FeeBumpTransaction.MIN_BASE_FEE)
+        .setTimeout(180)
+
+    val accountToSponsor = KeyPair.random()
+
+    txBuilder.addOperation(
+        BeginSponsoringFutureReservesOperation.Builder(accountToSponsor.accountId).build()
+    )
+    txBuilder.addOperation(
+        CreateAccountOperation.Builder(accountToSponsor.accountId, "0").build()
+    )
+    txBuilder.addOperation(
+        EndSponsoringFutureReservesOperation(accountToSponsor.accountId)
+    )
+
+    val transaction = txBuilder.build()
+    transaction.sign(questAccountKeys)
+    transaction.sign(accountToSponsor)
+    println("Executing tx..")
+    try {
+        val response = server.submitTransaction(transaction)
+        println("Success!")
+        println("txHash: ${response.hash}")
+        println("result: ${response.resultXdr.get()}")
+    } catch (e: Exception) {
+        println("Something went wrong!")
+        println(e.message)
+    }
+}
